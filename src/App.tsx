19b3e99dfdb53c15e7ab3950608e1c759bae7f49
chaki <aowp.contact@gmail.com>
2023-04-16 14:30:34 +0900

    アイコン変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import React, { useEffect } from 'react';
 import geoJson from './japan.geo.json';
 import sampleJson from './todouhuken.json';
 import { FeatureCollection, Feature } from 'geojson';
-import logo from './logo.svg';
-import logo2 from './logo.png';
+import logo from './heart.png';
 import './App.css';
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
   const height = 400; // 描画サイズ: 高さ
   const centerPos: [number, number] = [137.0, 38.2]; // 地図のセンター位置
   const scale = 1000; // 地図のスケール
+
 
 
   useEffect(() => {
@@ -24,7 +24,6 @@ function App() {
 
     // 地図をpathに投影(変換)
     const path: any = d3.geoPath().projection(projection);
-    const prefCoords = projection([142.3538, 43.0621]);
 
     // 都道府県ごとのデータを取得するAPIのエンドポイントとする
 
@@ -49,8 +48,8 @@ function App() {
       .attr(`width`, `100%`)
       .attr(`height`, `100%`);
 
-    
-      //地図描画
+
+    //地図描画
     svg
       .selectAll(`path`)
       .data(geoJson.features)
@@ -134,26 +133,36 @@ function App() {
         // d3.select(this).attr(`fill`, `#2566CC`);
         d3.select(this).attr(`stroke-width`, `0.25`);
       });
-
-      svg.selectAll(".icon")
-      .data(apiResponse.filter(function(d) {return d.flag;}))//仮でAPIレスポンスの成婚フラグみたいので判別
+    svg.selectAll(".icon")
+      .data(apiResponse.filter(function (d) { return d.flag; }))//仮でAPIレスポンスの成婚フラグみたいので判別
       .enter()
       .append("image")
+      .attr("class", "icon")
       .attr("xlink:href", logo)
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("transform", function(d) {
-        var centroid = path.centroid(geoJson.features.find(function(feature) {
+      .attr("type", "image/svg+xml")
+      .attr("width", function(d){
+        const width = d.population / 100000;
+        return width
+      })
+      .attr("height", function(d){
+        const height = d.population / 100000
+        return height;
+      })
+      .attr("transform", function (d) {
+        var centroid = path.centroid(geoJson.features.find(function (feature) {
           return feature.properties.name_ja === d.name;
         }));
-        return "translate(" + (centroid[0] - 5) + "," + (centroid[1] - 5) + ")";
+        if(d.name === '北海道'){
+          //ちと北海道だけ位置調整
+          return "translate(" + (centroid[0] - 25) + "," + (centroid[1] - 25) + ")";
+        }else{
+          return "translate(" + (centroid[0] - 5) + "," + (centroid[1] - 5) + ")";
+        }
       });
   })
   return (
     <div className="App">
-      
       <div id='map-container'>
-
       </div>
     </div>
   );
